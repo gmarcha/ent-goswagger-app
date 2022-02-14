05ab2b5ada3cfb4f51b4cee277151a1474e90be5
gmarcha <gmarcha@github.com>
2022-02-14 10:46:05 -0500

    update scripts: update goswagger script format

--- scripts/goswagger.sh
+++ scripts/goswagger.sh
@@ -12,4 +12,4 @@
 # goswagger generate server -A tutorAPI -f api/swagger.yaml -P models.Principal -t goswagger
 
 # After first generation, you could want to move `./goswagger/cmd/tutor-server/main.go` to `./cmd/tutor-server/main.go`,
-# to allow modifications on main to be persistant. Please add --exclude-main in go generate rule in ./goswagger/restapi/configure_tutor.go.
+# to allow modifications on main to be persistant. Please add `--exclude-main` in go generate rule in `./goswagger/restapi/configure_tutor.go`.
