b51465c3133343c6bff27a81407be42cb69413d8
gmarcha <gmarcha@github.com>
2022-02-18 23:39:11 -0500

    update user: update import path in init

--- internal/modules/user/init.go
+++ internal/modules/user/init.go
@@ -1,1 +1,10 @@
 package users
+
+import (
+	"github.com/gmarcha/ent-goswagger-app/internal/ent"
+	"github.com/gmarcha/ent-goswagger-app/internal/goswagger/restapi/operations"
+)
+
+func Init(api *operations.TutorAPI, db *ent.Client) {
+
+}
