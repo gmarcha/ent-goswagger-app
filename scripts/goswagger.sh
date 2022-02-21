a73c6423e71c742bb1d98cb8de4672d20b59d341
gmarcha <gmarcha@github.com>
2022-02-21 14:00:03 -0500

    update scripts: update goswagger script (execution path)

--- scripts/goswagger.sh
+++ scripts/goswagger.sh
@@ -1,14 +1,12 @@
 #! /bin/bash
 
 # - Installing goswagger:
-# dir=$(mktemp -d)
-# git clone https://github.com/go-swagger/go-swagger "$dir" 
-# cd "$dir"
-# go install ./cmd/swagger
+# git clone https://github.com/go-swagger/go-swagger
+# go install ./go-swagger/cmd/swagger
 
 # - Generate configuration files (and server):
 mkdir -p internal/goswagger
-swagger generate server -A tutor -f docs/swagger.yaml -P models.Principal -t internal/goswagger
+swagger generate server -A tutor -f config/specification.yaml -P models.Principal -t internal/goswagger
 
 # After first generation, you could want to move `./goswagger/cmd/tutor-server/main.go` to `./cmd/tutor-server/main.go`,
 # to allow modifications on main to be persistant. Please add `--exclude-main` in go generate rule in `./goswagger/restapi/configure_tutor.go`.
