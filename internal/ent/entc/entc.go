532ea3d988d59662962079d18281603248ebc288
gmarcha <gmarcha@github.com>
2022-02-22 15:23:42 -0500

    update ent: update code generation files and add openapi specification output (for data models)

--- internal/ent/entc/entc.go
+++ internal/ent/entc/entc.go
@@ -6,6 +6,7 @@ package main
 import (
 	"log"
 
+	"entgo.io/contrib/entoas"
 	"entgo.io/ent/entc"
 	"entgo.io/ent/entc/gen"
 )
@@ -15,14 +16,20 @@ import (
 // https://github.com/ent/ent/blob/master/entc/gen/template.go
 
 func main() {
-	err := entc.Generate("./schema", &gen.Config{
+	oas, err := entoas.NewExtension(
+		entoas.DefaultPolicy(entoas.PolicyExclude),
+	)
+	if err != nil {
+		log.Fatalf("creating entoas extension: %v", err)
+	}
+	err = entc.Generate("./schema", &gen.Config{
 		Templates: []*gen.Template{
 			gen.MustParse(
 				gen.NewTemplate("validate").
 					ParseFiles("template/validate.tmpl"),
 			),
 		},
-	})
+	}, entc.Extensions(oas))
 	if err != nil {
 		log.Fatal("running ent codegen:", err)
 	}
--- internal/ent/generate.go
+++ internal/ent/generate.go
@@ -1,3 +1,7 @@
 package ent
 
+import (
+	_ "entgo.io/contrib/entoas"
+)
+
 //go:generate go run -mod=mod entc/entc.go
