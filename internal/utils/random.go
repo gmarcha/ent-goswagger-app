b8e4817aae2270977da19d8e80adb7cf29cb1bd5
gmarcha <gmarcha@github.com>
2022-02-25 14:48:09 -0500

    update utils: add random string generator

---
+++ internal/utils/random.go
@@ -0,0 +1,20 @@
+package utils
+
+import (
+	"math/rand"
+	"time"
+)
+
+const charList = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func RandomString(size int) string {
+	b := make([]byte, size)
+	for i := range b {
+		b[i] = charList[rand.Intn(len(charList))]
+	}
+	return string(b)
+}
