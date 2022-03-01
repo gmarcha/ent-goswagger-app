c2c4320e650d797058ae49a75ed039e13202237f
gmarcha <gmarcha@github.com>
2022-03-01 17:32:40 -0500

    update modules: add all user handlers to api

--- internal/modules/user/init.go
+++ internal/modules/user/init.go
@@ -7,20 +7,22 @@ import (
 
 func Init(api *operations.TutorAPI, db *ent.Client) {
 
-	userService := &Service{User: db.User, Role: db.Role}
+	userService := &Service{User: db.User}
 
 	api.UserListUserHandler = &listUser{user: userService}
 	api.UserCreateUserHandler = &createUser{user: userService}
+
+	api.UserReadMeHandler = &readMe{user: userService}
+	api.UserUpdateMeHandler = &updateMe{user: userService}
+	api.UserDeleteMeHandler = &deleteMe{user: userService}
+	api.UserListMeEventsHandler = &listMeEvents{user: userService}
+	api.UserSubscribeMeHandler = &subscribeMe{user: userService}
+	api.UserUnsubscribeMeHandler = &unsuscribeMe{user: userService}
+
 	api.UserReadUserHandler = &readUser{user: userService}
 	api.UserUpdateUserHandler = &updateUser{user: userService}
 	api.UserDeleteUserHandler = &deleteUser{user: userService}
-	// api.UserListUserEventsHandler =
-	// api.UserSubscribeUserHandler =
-	// api.UserUnsubscribeUserHandler =
-
-	api.UserReadMeHandler = &readMe{user: userService}
-	// api.UserDeleteMeHandler =
-	// api.UserListMeEventsHandler =
-	// api.UserSubscribeMeHandler =
-	// api.UserUnsubscribeMeHandler =
+	api.UserListUserEventsHandler = &listUserEvents{user: userService}
+	api.UserSubscribeUserHandler = &subscribeUser{user: userService}
+	api.UserUnsubscribeUserHandler = &unsubscribeUser{user: userService}
 }
