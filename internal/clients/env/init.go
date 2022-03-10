f2924b19d2c2418481c6ae6e3cd9e428be4402b0
gmarcha <gmarcha@github.com>
2022-03-09 19:32:04 -0500

    update clients: update environment file parsing ((from command line argument)

--- internal/clients/env/init.go
+++ internal/clients/env/init.go
@@ -2,14 +2,17 @@ package env
 
 import (
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 )
 
-func Init(filename string) {
+func Init() {
 
+	env := os.Args[1]
+	filename := "./config/.env." + env
 	err := godotenv.Load(filename)
 	if err != nil {
-		log.Fatalf("Error loading .env file")
+		log.Fatalf("Error loading environment file")
 	}
 }
